cc5023a71a9b7e5bfa4b72dcb3b4ba048cdccb28
Чурюмов <aachuryumov@advent.local>
2018-10-09 17:19:09 +0300

    working with os.walk

--- 1_pojects/current/copypaster.py
+++ 1_pojects/current/copypaster.py
@@ -1,18 +1,43 @@
-import os
+import os     # for functions: walk, path.join, remove, path.getsize, path.get[cam]time, mkdir
+import shutil # copy, move
+
+def PrintFrame(inStr):
+  print("_______________________________________")
+  print("* " + inStr + " ->")
+
+# void for show cortege
+def ShowCrtg(inDir, inAbout):
+  PrintFrame("Start show cortege: " + inAbout)
+  # !!!WARNING NOT REALIZATION TEST OF DIR
+  for inCortege in os.walk(inDir): # walking in the directoryes
+    print(inCortege)
+  print("* End show cortege")
+
+def CopyPaster(inFrom, inTo):
+  print("* Start copy from (" + inFrom + ")")
+  print("* In the direcory (" + inTo + ")")
+  shutil.copy()
 
 def RunCp():
-  print("***************************************")
-  print("Hello! Do you want copying your folder?")
-  print("Than Let's go")
+  print("*****************************************")
+  print("* Hello! Do you want copying your folder?")
+  print("* Than Let's go")
+
   try:
-    print("Write a directory - from: ")
+    #  1. show whole
+    print("* Write a directory - from: ")
     dir_from = input()
-    for inCartage in os.walk(dir_from):
-      print(inCartage)
-    #print("Write a directory - to  : ")
-    #to_dir = input()
-    #print("Nice! Please wait, the copypaster is working...")
-    
+    #show cortege
+    ShowCrtg(dir_from, "from")        
+
+    print("* Write a directory - to: ")
+    dir_to = input()
+    #show cortege
+    ShowCrtg(dir_to,"to")
+    PrintFrame("Start copy catalog in to")
+
+    # 2. Copy
+
   except OSError:
     print("We are have bad news, a directory which you was writed is false...try again!")
     
