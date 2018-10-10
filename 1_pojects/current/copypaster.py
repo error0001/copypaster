892dd6ee450b26449e1135a3052e78b65deac642
Чурюмов <aachuryumov@advent.local>
2018-10-10 11:39:00 +0300

    version 0.0.1

--- 1_pojects/current/copypaster.py
+++ 1_pojects/current/copypaster.py
@@ -1,6 +1,55 @@
+'''
+Author  : error0001
+Version : 0.0.1
+Name    : CopyPaster
+'''
 import os     # for functions: walk, path.join, remove, path.getsize, path.get[cam]time, mkdir
 import shutil # copy, move
 
+def CopyFromTo():
+  mainDir = input("* Write the MAIN dir:")
+
+  try:
+    os.chdir(mainDir)
+  except OSError:
+    print("* Error of \"write MAIN dir\"")
+
+  fromDir = input("* Write the FROM dir:")
+  toDir = input("* Write the TO   dir:")
+  try:
+    shutil.copytree(fromDir,toDir)
+  except OSError:
+    print("* Error of \"copytree or writed dirs\"")
+    
+
+def RunCp():
+  print("**********************************************************")
+  print("* ________________________Hello!__________________________")
+  print("* This script needed to copy, move rename folders or files")
+  print("**********************************************************")
+  print("* ______________________Instruction_______________________")
+  print("* __________You must write key of concret task____________")
+  print("* cft  - Copy From(Write E:\\folder0) To(Write C:\\pd\\ds)")
+  print("* none - testing...")
+  print("* none - testing...")
+  print("**********************************************************")
+  
+  # Get key 
+  inKey = input("* Change the key of work: ") 
+  try:
+    if inKey == 'cft':
+      CopyFromTo() 
+  except OSError:
+    print('Error input key')
+
+# main
+if __name__ == '__main__':
+  RunCp()
+
+
+
+
+'''
 def PrintFrame(inStr):
   print("_______________________________________")
   print("* " + inStr + " ->")
@@ -13,34 +62,30 @@ def ShowCrtg(inDir, inAbout):
   for inCortege in os.walk(inDir): # walking in the directoryes
     print(inCortege)
   print("* End show cortege")
+'''
 
-def CopyPaster(inFrom, inTo):
-  print("* Start copy from (" + inFrom + ")")
-  print("* In the direcory (" + inTo + ")")
-  os.path.join('\'' + inFrom, inTo)  # copy folders into new dir
-
-def RunCp():
-  print("*****************************************")
-  print("* Hello! Do you want copying your folder?")
-  print("* Than Let's go")
-
+'''
   try:
-    #  1. show whole
-    print("* Write a directory - from: ")
-    dir_from = input()
-    #show cortege
-    ShowCrtg(dir_from, "from")        
-
-    print("* Write a directory - to: ")
-    dir_to = input()
-    #show cortege
-    ShowCrtg(dir_to,"to")
+    # 1. show whole
+    #print("* Write a directory - from: ")
+    #dir_from = input()
+    #ShowCrtg(dir_from, "from")        
+    #
+    #print("* Write a directory - to: ")
+    #dir_to = input()
+    #ShowCrtg(dir_to,"to")
 
     # 2. Copy
-    PrintFrame("Start copy catalog in to")
-    CopyPaster(dir_from, dir_to)
+    #PrintFrame("Start copy catalog in to")
+    #print("* Write the main catalog: ")
+    #main_dir = input()
+
+    #CopyPaster()#main_dir ,dir_from, dir_to) 
+
+  
+    #os.chdir('C:\\')
+
+    #shutil.copy('C:\\spam.txt', 'C:\\delicious')
   except OSError:
     print("We are have bad news, a directory which you was writed is false...try again!")
-    
-if __name__ == '__main__':
-  RunCp()
+  '''  
